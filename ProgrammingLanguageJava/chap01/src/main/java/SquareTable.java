c034234a09056e21141d8d8758410c5ea547a75d
YumiIwasa <yumi.iwasa@a-saas.com>
2020-02-09 23:01:43 +0900

    プログラミング言語Java第4版　練習問題1.4 50までの平方表

---
+++ ProgrammingLanguageJava/chap01/src/main/java/SquareTable.java
@@ -0,0 +1,10 @@
+public class SquareTable {
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println("乗数" + ": " + "平方数");
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(n + ": " + n * n);
+            n++;
+        }
+    }
+}
