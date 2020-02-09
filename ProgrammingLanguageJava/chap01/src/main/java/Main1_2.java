53eb9397f9772ee335d6badb3aa80650d0a78526
YumiIwasa <yumi.iwasa@a-saas.com>
2020-02-09 23:27:41 +0900

    プログラミング言語Java第4版　練習問題1.6 タイトルに名前付文字列定数を使用するように変更

--- ProgrammingLanguageJava/chap01/src/main/java/Main1_2.java
+++ ProgrammingLanguageJava/chap01/src/main/java/Main1_2.java
@@ -1,9 +1,11 @@
 public class Main1_2 {
+    static final String JYOUSU = "乗数";
+    static final String HEIHOUSU = "平方数";
     public static void main(String[] args) {
         int lo = 1;
         int hi = 1;
         int count = 1;
-        System.out.println(lo);
+        System.out.println(JYOUSU + ": " + HEIHOUSU);
         while (hi < 50) {
             System.out.println(count + ":" + hi);
             hi = lo + hi;
